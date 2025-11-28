7a2038bde44181a4bd27c905242db5166b53469d
Guilherme Tavares <gui.tavares2002.contato@gmail.com>
2025-11-27 21:02:32 -0300

    Update files

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const session = require('express-session');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views','views');
 
+const User = require('./models/user');
+
 const menuRoutes = require('./routes/menu');
 const cadastroClienteRoutes = require('./routes/cadastroCliente');
-const authRoutes = require('./routes/auth')
+const authRoutes = require('./routes/auth');
+
 
 
 app.use(express.static(__dirname + '/public'));
@@ -25,5 +30,25 @@ app.use(cadastroClienteRoutes);
 app.use(authRoutes);
 
 
-app.listen(3000);
+mongoose
+.connect(process.env.MONGODB_URI)
+.then(result => {
+    User.findOne()
+        .then(user => {
+            // It will be removed later
+            if(!user) {
+                const user = new User({
+                    email: process.env.ADMIN_EMAIL,
+                    password: process.env.ADMIN_PASSWORD,
+                    name: process.env.ADMIN_NAME
+                });
+                user.save();
+            }
+        })
+        .catch(err => console.log(err));
+    app.listen(3000);
+})
+.catch(err => console.log(err));
+
+
 
